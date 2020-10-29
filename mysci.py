8dd0f5bd47b2e21a78249ea60b05bfa0118d671e
Kelly Nunez Ocasio <knocasio@cheyenne5.cheyenne.ucar.edu>
2020-10-29 16:07:37 -0600

    Refactoring data parsing code

--- mysci.py
+++ mysci.py
@@ -1,5 +1,17 @@
+
+# Column names and column indices to read
+
+columns = {'date':0, 'time':1, 'tempout':2}
+
+#Dta types for each column (only if non-string) 
+
+types = {'tempout': float}
+
 #Initislize my data variable 
-data = {'date':[], 'time':[], 'tempout':[]} # list 
+data = {} # list
+
+for column in columns:
+    data[column] = [] 
 
 #time = data['time']
 
@@ -28,8 +40,9 @@ with open(filename, 'r') as datafile: # with the file open, i would like to read
     # Read and parse the rest of the file 
     for line in datafile:
         split_line = line.split() 
-        data['date'].append(split_line[0])
-        data['time'].append(split_line[1])
-        data['tempout'].append(float(split_line[2]))
-
+        for column in columns: 
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
 
